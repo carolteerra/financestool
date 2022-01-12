44173c5054161f51d82f513db958a2f2e3abc22d
Caroline Terra <caroltv94@gmail.com>
2022-01-12 20:03:47 -0300

    Base project upload
    Uploads Spring Boot project with dependencies and config.

---
+++ financestool/financestool/src/main/java/com/carolteerra/financestool/FinancestoolApplication.java
@@ -0,0 +1,13 @@
+package com.carolteerra.financestool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancestoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancestoolApplication.class, args);
+	}
+
+}
---
+++ financestool/financestool/src/test/java/com/carolteerra/financestool/FinancestoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolteerra.financestool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancestoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
