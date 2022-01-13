bad69a13632907bbedbfa636c647d840984dbb89
Caroline Terra <caroltv94@gmail.com>
2022-01-13 14:50:12 -0300

    Minor changes

--- financestool/financestool/src/main/java/com/carolteerra/financestool/FinancestoolApplication.java
+++ financestool/financestool/src/main/java/com/carolteerra/financestool/FinancestoolApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.beans.JavaBean;
 
 @SpringBootApplication
 @ComponentScan("com.carolteerra.financestool.model.repositories")
-public class FinancestoolApplication {
+public class FinancestoolApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancestoolApplication.class, args);
